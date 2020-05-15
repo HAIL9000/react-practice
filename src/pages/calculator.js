e64fa50362ecb0e0a21c0952c85bb593bcfdfd51
Hailee Kenney <hailee.kenney@gmail.com>
2020-05-15 10:01:27 -0700

    Add calculator buttons

--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -4,10 +4,43 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import calculatorStyles from "./calculator.module.css"
+
+function CalcButton(props) {
+    return(
+        <button className={calculatorStyles.button}>
+            {props.children}
+        </button>
+    )
+}
+
+function Calculator() {
+    return(
+        <div>
+            <div>
+                <CalcButton>1</CalcButton>
+                <CalcButton>2</CalcButton>
+                <CalcButton>3</CalcButton>
+            </div>
+            <div>
+                <CalcButton>4</CalcButton>
+                <CalcButton>5</CalcButton>
+                <CalcButton>6</CalcButton>
+            </div>
+            <div>
+                <CalcButton>7</CalcButton>
+                <CalcButton>8</CalcButton>
+                <CalcButton>9</CalcButton>
+            </div>
+        </div>
+    )
+}
+
 const CalculatorPage = () => (
     <Layout>
         <SEO title="Calculator" />
         <h2>~*~ Calculator ~*~</h2>
+        <Calculator />
         <Link  to="/">Go Home</Link>
     </Layout>
 )
