28b85f3ecf420a0a843fbfc638d4107c12dfa7c1
Hailee Kenney <hailee.kenney@gmail.com>
2020-05-15 18:31:58 -0700

    Accept basic input
    Accept and clear user input.
    
    Can't do any math yet.

--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -6,34 +6,89 @@ import SEO from "../components/seo"
 
 import calculatorStyles from "./calculator.module.css"
 
+// Function syntax component
 function CalcButton(props) {
     return(
-        <button className={calculatorStyles.button}>
-            {props.children}
+        <button className={calculatorStyles.button} onClick={props.onClick}>
+            {props.id}
         </button>
     )
 }
 
-function Calculator() {
-    return(
-        <div>
+// "Class" syntax component
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userInput: [],
+        };
+    }
+
+    handleClick(id) {
+        const userInput = this.state.userInput
+        userInput.push(id)
+
+        if(id === "CLEAR"){
+            this.setState(
+                {
+                    userInput: [],
+                }
+            );
+
+        } else {
+            this.setState(
+                {
+                    userInput: userInput,
+                }
+            );
+        }
+    }
+
+    renderButton(id) {
+        return (
+            <CalcButton
+                id={id}
+                onClick= {() => this.handleClick(id)}
+            />
+        );
+    }
+
+    render() {
+        const screen = "Input: " + this.state.userInput.join('')
+        return(
             <div>
-                <CalcButton>1</CalcButton>
-                <CalcButton>2</CalcButton>
-                <CalcButton>3</CalcButton>
+                {screen}
+                <div>
+                    {this.renderButton(1)}
+                    {this.renderButton(2)}
+                    {this.renderButton(3)}
+                    {this.renderButton('+')}
+                </div>
+                <div>
+                    {this.renderButton(4)}
+                    {this.renderButton(5)}
+                    {this.renderButton(6)}
+                    {this.renderButton('-')}
+                </div>
+                    {this.renderButton(7)}
+                    {this.renderButton(8)}
+                    {this.renderButton(9)}
+                    {this.renderButton('x')}
+                <div>
+                </div>
+                    {this.renderButton(0)}
+                    {this.renderButton('.')}
+                    {this.renderButton('=')}
+                    {this.renderButton('÷')}
+                <div>
+                </div>
+                    {this.renderButton('CLEAR')}
+                <div>
+                </div>
             </div>
-            <div>
-                <CalcButton>4</CalcButton>
-                <CalcButton>5</CalcButton>
-                <CalcButton>6</CalcButton>
-            </div>
-            <div>
-                <CalcButton>7</CalcButton>
-                <CalcButton>8</CalcButton>
-                <CalcButton>9</CalcButton>
-            </div>
-        </div>
-    )
+        );
+    }
 }
 
 const CalculatorPage = () => (
