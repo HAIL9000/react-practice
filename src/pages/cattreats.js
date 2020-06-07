408414b2a7fa0da8fa2a9de42476ad8aaa4da685
Hailee Kenney <hailee.kenney@gmail.com>
2020-06-07 10:55:38 -0700

    Add basic cat
    Add some basic components so that we can have a single
    cat which can receive treats.

--- src/pages/cattreats.js
+++ src/pages/cattreats.js
@@ -1,13 +1,48 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+function Cafe() {
+    const [treatCount, setTreatCount] = useState(0);
+
+    function giveTreat() {
+        setTreatCount(treatCount + 1);
+    }
+
+    return <div>
+        <Cat
+            name="Atton"
+            treatCount={treatCount}
+        />
+        <TreatButton
+            handleClick={giveTreat}
+        />
+    </div>
+
+}
+
+function Cat(props) {
+    return <div>
+       <div>Name: {props.name}</div>
+       <div>Treat Count: {props.treatCount}</div>
+    </div>
+}
+
+function TreatButton(props) {
+    const handleClick = () => {props.handleClick()}
+    return <button
+        onClick={handleClick}>
+        Give Treat!
+        </button>
+}
+
 const CatPage = () => (
     <Layout>
         <SEO title="Cat Treat Cafe" />
         <h2>~*~ Cat Treat Cafe ~*~</h2>
+        <Cafe />
         <Link to="/">Go Home</Link>
     </Layout>
 )
