c4a05edde53b8abd56030dc31bb9d8357cdd111e
Hailee Kenney <hailee.kenney@gmail.com>
2020-06-07 21:29:09 -0700

    Support multiple cats for real
    Be sure that when state is updated, new arrays
    are created so that react will know to update the
    DOM. Also start using prettier to format code.

--- src/pages/cattreats.js
+++ src/pages/cattreats.js
@@ -1,75 +1,82 @@
-import React, { useState } from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 function Cafe() {
-    const [catTracker, setCatTracker] = useState([]);
+  const [catTracker, setCatTracker] = useState([]);
 
-    function giveTreat(catName) {
-        let catIndex = catTracker.findIndex(x => x.name === catName);
-        let cats = catTracker;
+  function giveTreat(catName) {
+    const catIndex = catTracker.findIndex((x) => x.name === catName);
 
-        if(catIndex === -1) {
-            let newCat = {
-                name: catName,
-                treatCount: 0
-            }
-            cats.push(newCat);
-            setCatTracker(cats);
+    if (catIndex === -1) {
+      const newCat = {
+        name: catName,
+        treatCount: 1,
+      };
+      setCatTracker([...catTracker, newCat]);
+    } else {
+      const newCats = catTracker.map((cat, index) => {
+        if (index === catIndex) {
+          return { ...cat, treatCount: cat.treatCount + 1 };
         } else {
-            let cat = cats[catIndex];
-            cat.treatCount = cat.treatCount + 1;
-            cats[catIndex] = cat;
-            setCatTracker(cats);
+          return cat;
         }
+      });
+      setCatTracker(newCats);
     }
+  }
 
-    function getCount(catName) {
-        let cat = catTracker.find(x => x.name === catName);
-        if(cat) {
-            return cat.treatCount;
-        } else {
-            return 0;
-        }
+  function getCount(catName) {
+    let cat = catTracker.find((x) => x.name === catName);
+    if (cat) {
+      return cat.treatCount;
+    } else {
+      return 0;
     }
+  }
 
-    return <div>
+  return (
+    <div>
+      <p>
         <Cat
-            name="Atton"
-            treatCount={getCount("Atton")}
+          name="Atton"
+          treatCount={getCount("Atton")}
+          giveTreat={giveTreat}
         />
-        <TreatButton
-            handleClick={giveTreat}
-            catName="Atton"
-        />
+      </p>
+      <p>
+        <Cat name="Unix" treatCount={getCount("Unix")} giveTreat={giveTreat} />
+      </p>
     </div>
-
+  );
 }
 
 function Cat(props) {
-    return <div>
-       <div>Name: {props.name}</div>
-       <div>Treat Count: {props.treatCount}</div>
+  return (
+    <div>
+      <div>Name: {props.name}</div>
+      <div>Treat Count: {props.treatCount}</div>
+      <TreatButton handleClick={props.giveTreat} catName={props.name} />
     </div>
+  );
 }
 
 function TreatButton(props) {
-    const handleClick = () => {props.handleClick(props.catName)}
-    return <button
-        onClick={handleClick}>
-        Give Treat!
-        </button>
+  const handleClick = () => {
+    props.handleClick(props.catName);
+  };
+  return <button onClick={handleClick}>Give Treat!</button>;
 }
 
 const CatPage = () => (
-    <Layout>
-        <SEO title="Cat Treat Cafe" />
-        <h2>~*~ Cat Treat Cafe ~*~</h2>
-        <Cafe />
-        <Link to="/">Go Home</Link>
-    </Layout>
-)
+  <Layout>
+    <SEO title="Cat Treat Cafe" />
+    <h2>~*~ Cat Treat Cafe ~*~</h2>
+    <Cafe />
+    <Link to="/">Go Home</Link>
+  </Layout>
+);
 
-export default CatPage
+export default CatPage;
