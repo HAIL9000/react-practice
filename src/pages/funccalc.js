c0707f545bb4f0ec53246a73df2b93c3edb0cdc6
Hailee Kenney <hailee.kenney@gmail.com>
2020-06-12 10:14:40 -0700

    Clean up calculator 2
    Get rid of some unneeded cruft

--- src/pages/funccalc.js
+++ src/pages/funccalc.js
@@ -1,202 +1,128 @@
-import React, { useState } from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-import calclutorStyles from "./calculator.module.css"
+import calclutorStyles from "./calculator.module.css";
 
 function Calculator() {
+  const [leftOp, setLeftOp] = useState("");
+  const [operator, setOperator] = useState(null);
+  const [rightOp, setRightOp] = useState("");
+  const [displayIndex, setDisplayIndex] = useState("");
 
-    const [leftOp, setLeftOp] = useState("");
-    const [operator, setOperator] = useState(null);
-    const [rightOp, setRightOp] = useState("");
-    const [displayIndex, setDisplayIndex] = useState("");
+  function clearInput() {
+    setLeftOp("");
+    setRightOp("");
+    setDisplayIndex("");
+    setOperator(null);
+  }
 
-    function clearInput() {
-        setLeftOp("");
-        setRightOp("");
-        setDisplayIndex("");
-        setOperator(null);
+  function storeUserInput(input) {
+    let updatedInput;
+    if (operator) {
+      updatedInput = rightOp + input;
+      setRightOp(updatedInput);
+    } else {
+      updatedInput = leftOp + input;
+      setLeftOp(updatedInput);
+    }
+    setDisplayIndex(updatedInput);
+  }
+
+  function doMath() {
+    let numLeft = Number(leftOp);
+    let numRight = Number(rightOp);
+    let result;
+
+    switch (operator) {
+      case "+":
+        result = numLeft + numRight;
+        break;
+      case "-":
+        result = numLeft - numRight;
+        break;
+      case "x":
+        result = numLeft * numRight;
+        break;
+      case "÷":
+        result = numLeft / numRight;
+        break;
+      default:
+        console.log("Operator error!!");
     }
 
-    function storeUserInput(input){
-        let updatedInput;
-        if(operator) {
-           updatedInput = rightOp + input;
-           setRightOp(updatedInput);
-        } else {
-            updatedInput = leftOp + input;
-            setLeftOp(updatedInput);
-        }
-        setDisplayIndex(updatedInput);
-    }
+    setLeftOp(result + "");
+    setRightOp("");
+    setDisplayIndex(result + "");
+    setOperator("");
+  }
 
-    function doMath(){
-        let numLeft = Number(leftOp);
-        let numRight = Number(rightOp);
-        let result;
-
-        switch(operator){
-        case "+":
-            result = numLeft + numRight;
-            break;
-        case "-":
-            result = numLeft - numRight;
-            break;
-        case "x":
-            result = numLeft * numRight;
-            break;
-        case "÷":
-            result = numLeft / numRight;
-            break;
-        default:
-            console.log("Operator error!!")
-
-    }
-
-        setLeftOp(result + "");
-        setRightOp("");
-        setDisplayIndex(result + "");
-        setOperator("");
-    }
-
-    return <div>
-                <div>{displayIndex}</div>
-                <CalcGrid
-                    storeUserInput={storeUserInput}
-                    operator={operator}
-                    setOperator={setOperator}
-                    clearInput={clearInput}
-                    doMath={doMath}
-                />
-            </div>
-
+  return (
+    <div>
+      <div>{displayIndex}</div>
+      <CalcGrid
+        storeUserInput={storeUserInput}
+        operator={operator}
+        setOperator={setOperator}
+        clearInput={clearInput}
+        doMath={doMath}
+      />
+    </div>
+  );
 }
 
 function CalcGrid(props) {
-    const {storeUserInput, operator, setOperator, clearInput, doMath} = props
-    return <div>
-        <div>
-            <NumButton
-                id="1"
-                onClick={storeUserInput}
-            />
-            <NumButton
-                id="2"
-                onClick={storeUserInput}
-            />
-            <NumButton
-                id="3"
-                onClick={storeUserInput}
-            />
-            <OpButton
-                id="+"
-                onClick={setOperator}
-                leftOp={operator}
-            />
-        </div>
-        <div>
-            <NumButton
-                id="4"
-                onClick={storeUserInput}
-            />
-            <NumButton
-                id="5"
-                onClick={storeUserInput}
-            />
-            <NumButton
-                id="6"
-                onClick={storeUserInput}
-            />
-            <OpButton
-                id="-"
-                onClick={setOperator}
-                leftOp={operator}
-            />
-        </div>    
-        <div>
-            <NumButton
-                id="7"
-                onClick={storeUserInput}
-            />
-            <NumButton
-                id="8"
-                onClick={storeUserInput}
-            />
-            <NumButton
-                id="9"
-                onClick={storeUserInput}
-            />
-            <OpButton
-                id="x"
-                onClick={setOperator}
-                leftOp={operator}
-            />
-        </div>    
-        <div>
-            <NumButton
-                id="0"
-                onClick={storeUserInput}
-            />
-            <NumButton
-                id="."
-                onClick={storeUserInput}
-            />
-            <GenButton
-                id="="
-                onClick={doMath}
-            />
-            <OpButton
-                id="÷"
-                onClick={setOperator}
-                leftOp={operator}
-            />
-        </div>    
-        <div>
-            <GenButton
-                id="CLEAR"
-                onClick={clearInput}
-            />
-        </div>
+  const { storeUserInput, setOperator, clearInput, doMath } = props;
+  return (
+    <div>
+      <div>
+        <GenButton id="1" handleClick={() => storeUserInput("1")} />
+        <GenButton id="2" handleClick={() => storeUserInput("2")} />
+        <GenButton id="3" handleClick={() => storeUserInput("3")} />
+        <GenButton id="+" handleClick={() => setOperator("+")} />
+      </div>
+      <div>
+        <GenButton id="4" handleClick={() => storeUserInput("4")} />
+        <GenButton id="5" handleClick={() => storeUserInput("5")} />
+        <GenButton id="6" handleClick={() => storeUserInput("6")} />
+        <GenButton id="-" handleClick={() => setOperator("-")} />
+      </div>
+      <div>
+        <GenButton id="7" handleClick={() => storeUserInput("7")} />
+        <GenButton id="8" handleClick={() => storeUserInput("8")} />
+        <GenButton id="9" handleClick={() => storeUserInput("9")} />
+        <GenButton id="x" handleClick={() => setOperator("x")} />
+      </div>
+      <div>
+        <GenButton id="0" handleClick={() => storeUserInput("0")} />
+        <GenButton id="." handleClick={() => storeUserInput(".")} />
+        <GenButton id="=" handleClick={() => doMath()} />
+        <GenButton id="÷" handleClick={() => setOperator("÷")} />
+      </div>
+      <div>
+        <GenButton id="CLEAR" handleClick={() => clearInput()} />
+      </div>
     </div>
-
-}
-
-function NumButton(props) {
-    const handleClick = () => {props.onClick(props.id)}
-    return <button
-             className={calclutorStyles.button}
-             onClick={handleClick}>
-             {props.id}
-           </button>
-}
-
-function OpButton(props) {
-    const handleClick = () => {props.onClick(props.id)}
-    return <button 
-             className={calclutorStyles.button}
-             onClick={handleClick}>
-             {props.id}
-           </button>
+  );
 }
 
 function GenButton(props) {
-    const handleClick = () => {props.onClick()}
-    return <button
-             className={calclutorStyles.button}
-             onClick={handleClick}>
-             {props.id}
-           </button>
+  return (
+    <button className={calclutorStyles.button} onClick={props.handleClick}>
+      {props.id}
+    </button>
+  );
 }
 
 const CalculatorPagePart2 = () => (
-    <Layout>
-        <SEO title="Calculator Part 2" />
-        <h2>~*~ Calculator Part 2~*~</h2>
-        <Calculator />
-        <Link  to="/">Go Home</Link>
-    </Layout>
-)
+  <Layout>
+    <SEO title="Calculator Part 2" />
+    <h2>~*~ Calculator Part 2~*~</h2>
+    <Calculator />
+    <Link to="/">Go Home</Link>
+  </Layout>
+);
 
-export default CalculatorPagePart2
+export default CalculatorPagePart2;
